63148d133acee966b47987344ab371fdbdb62d72
Timothy Chang <tchang135@gmail.com>
2023-01-04 15:04:15 -0500

    finished twosum

---
+++ skeleton/phase_1_arrays.js
@@ -0,0 +1,25 @@
+// Array.prototype.uniq = function() {
+//     let uniqueEle = []
+//     for (let i = 0 ;i < this.length;i++) {
+//         if (!uniqueEle.includes(this[i])) {
+//             uniqueEle.push(this[i])
+//         } 
+//     }
+//     return uniqueEle
+// }
+
+// Array.prototype.twoSum = function() {
+//     const sums = []
+//     for (let i = 0; i < this.length; i++) {
+//         for (let j = 0; j < this.length; j++) {
+//             if ((j > i) && (this[i] + this[j] === 0)) {
+//                 sums.push([i, j]);
+//             }
+//         }
+//     }
+
+//     return sums
+// }
+
+
+
